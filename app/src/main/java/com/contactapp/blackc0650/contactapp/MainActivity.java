6eb4add443f971a6b4daf443a4869b7e318329fe
Soccervolley5 <BlackC0650@sduhsd.lan>
2018-05-22 09:00:43 -0700

    Created the Database for ContactApp
    Created the DatabaseHelper class.

--- app/src/main/java/com/contactapp/blackc0650/contactapp/MainActivity.java
+++ app/src/main/java/com/contactapp/blackc0650/contactapp/MainActivity.java
@@ -2,12 +2,15 @@ package com.contactapp.blackc0650.contactapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
-
+    private DatabaseHelper databaseHelper;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        databaseHelper = new DatabaseHelper(this);
+        Log.d("ContactApp","MainActivity: instantiated DatabaseHelper");
     }
 }
