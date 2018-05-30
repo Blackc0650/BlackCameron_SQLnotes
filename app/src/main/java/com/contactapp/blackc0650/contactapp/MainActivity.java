a1632739ed06bcdde58a3bde3c02b6b94642123a
Soccervolley5 <BlackC0650@sduhsd.lan>
2018-05-30 08:56:13 -0700

    Fixed the Gradle
    App is done by requirements

--- app/src/main/java/com/contactapp/blackc0650/contactapp/MainActivity.java
+++ app/src/main/java/com/contactapp/blackc0650/contactapp/MainActivity.java
@@ -1,16 +1,15 @@
 package com.contactapp.blackc0650.contactapp;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.database.Cursor;
-import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     private DatabaseHelper databaseHelper;
--- app/src/main/java/com/contactapp/blackc0650/contactapp/SearchActivity.java
+++ app/src/main/java/com/contactapp/blackc0650/contactapp/SearchActivity.java
@@ -6,6 +6,7 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
+
 public class SearchActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
